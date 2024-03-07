1c702fa4ba3bd4318815aed12668182c376e3db9
Mohammed El Hamdaoui <melhamdaoui811@gmail.com>
2024-03-07 16:58:42 +0100

    done download

--- checkout.php
+++ checkout.php
@@ -1,5 +1,5 @@
 <?php
-if (!isset($_POST["name"])) {
+if (!isset($_POST["type"])) {
     header("location: /icnf-l/");
     exit();
 }
@@ -11,14 +11,110 @@ if (!isset($_POST["name"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/checkout.css">
+
+    <title>checkout</title>
 </head>
 
-<body>
-    <form action="./add-order" method="post">
-        <input type="text" name="firstName" placeholder="Lirst Name" />
-        <input type="text" name="lastName" placeholder="Last Name" />
-    </form>
+<body class="body-check">
+
+    <div class="container card-conts">
+        <div class="left">
+            <div>
+                <img class="card-logo" src="./assets/icons/icon1.svg" alt="" srcset="">
+            </div>
+            <div class="card-content">
+                <div class="card-title">
+                    <?php echo $_POST["type"]; ?>
+                </div>
+                <div class="desc">
+                    Offre : <?php echo $_POST["type"]; ?>
+                </div>
+                <div class="price">
+                    <p>total : </p>
+                    <h1>€ <?php echo $_POST["total"]; ?></h1>
+                </div>
+                <form class="card-form">
+                    <div class="inp-group">
+                        <input class="fname" type="text" required placeholder="First name" />
+                        <input class="lname" type="text" required placeholder="Last name" />
+                    </div>
+                    <input class="cne" type="text" required placeholder="CNE" />
+                </form>
+            </div>
+
+        </div>
+        <div class="right">
+            <div class="div">
+                <h1>TOTAL</h1>
+            </div>
+            <div class="div">
+                <?php
+
+                if (isset($_POST["450"]) && $_POST["type"] == "Before 10th May 2024")
+                    echo "<div class='elem'>
+                    <p>Regular Participants</p>
+                    <p>€ 450</p>
+                    </div>";
+                if (isset($_POST["70"]) && $_POST["type"] == "Before 10th May 2024")
+                    echo "<div class='elem'>
+                    <p>Social Event</p>
+                    <p>€ 70</p>
+                    </div>";
+                if (isset($_POST["300"]) && $_POST["type"] == "Before 10th May 2024")
+                    echo "<div class='elem'>
+                    <p>Student</p>
+                    <p>€ 300</p>
+                    </div>";
+                if (isset($_POST["200"]) && $_POST["type"] == "Before 10th May 2024")
+                    echo "<div class='elem'>
+                    <p>Accompanying person</p>
+                    <p>€ 200</p>
+                    </div>";
+
+                //the other one
+                if (isset($_POST["550"]) && $_POST["type"] == "From 15th June 2024")
+                    echo "<div class='elem'>
+                    <p>Regular Participants</p>
+                    <p>€ 550</p>
+                    </div>";
+                if (isset($_POST["70"]) && $_POST["type"] == "From 15th June 2024")
+                    echo "<div class='elem'>
+                    <p>Social Event</p>
+                    <p>€ 70</p>
+                    </div>";
+                if (isset($_POST["400"]) && $_POST["type"] == "From 15th June 2024")
+                    echo "<div class='elem'>
+                    <p>Student</p>
+                    <p>€ 400</p>
+                    </div>";
+                if (isset($_POST["200"]) && $_POST["type"] == "From 15th June 2024")
+                    echo "<div class='elem'>
+                    <p>Accompanying person</p>
+                    <p>€ 200</p>
+                    </div>";
+                ?>
+            </div>
+            <div class="div">
+                <div class="elem">
+                    <p>Total</p>
+                    <p class="total">€ <?php echo $_POST["total"]; ?></p>
+                </div>
+            </div>
+            <di class="btns-group">
+                <button class="download">DOWNLOAD</button>
+                <button class="upload">UPLOAD</button>
+        </div>
+    </div>
+    </div>
+
+    <script src="https://unpkg.com/jspdf-invoice-template@1.4.0/dist/index.js"></script>
+    <script src="./script.js"></script>
+    <script type="text/javascript">
+        var data = <?php echo json_encode($_POST) ?>;
+    </script>
+    <script src="./checkout.js"></script>
 </body>
 
 </html>
--- script.js
+++ script.js
@@ -17,6 +17,7 @@ dropDown.addEventListener("click", () => {
 
 localStorage.setItem("price", JSON.stringify({ regPrice: 0, studPrice: 0 }));
 
+
 const checkR450 = document.getElementById("r450");
 let	R450 = 0;
 const checkR70 = document.getElementById("r70");
@@ -37,11 +38,24 @@ let S400 = 0;
 
 const req = document.getElementById("r-price");
 const std = document.getElementById("s-price");
+const total1 =  document.getElementById("total1");
+const total2 =  document.getElementById("total2");
 
-window.addEventListener("load", () => {
+window.addEventListener("hashchange", () => {
+	checkR200.checked = false;
+	checkR300.checked = false;
+	checkR450.checked = false;
+	checkR70.checked = false;
+	checkS200.checked = false;
+	checkS400.checked = false;
+	checkS550.checked = false;
+	checkS70.checked = false;
+	localStorage.removeItem("price")
+	localStorage.setItem("price", JSON.stringify({ regPrice: 0, studPrice: 0 }));
 	let data = JSON.parse(localStorage.getItem("price"));
 	req.innerText = "€ " + data.regPrice;
 	std.innerText = "€ " + data.studPrice;
+	console.log("ha")
 })
 
 checkR70.addEventListener("change", (e) => {
@@ -58,6 +72,7 @@ checkR70.addEventListener("change", (e) => {
 		R70 = 0;
 	}
 	req.innerText = "€ " + data.regPrice;
+	total1.value = data.regPrice;
 	localStorage.setItem("price", JSON.stringify(data));
 })
 
@@ -75,6 +90,7 @@ checkR200.addEventListener("change", (e) => {
 		R200 = 0;
 	}
 	req.innerText = "€ " + data.regPrice;
+	total1.value = data.regPrice;
 	localStorage.setItem("price", JSON.stringify(data));
 })
 
@@ -92,6 +108,7 @@ checkR300.addEventListener("change", (e) => {
 		R300 = 0;
 	}
 	req.innerText = "€ " + data.regPrice;
+	total1.value = data.regPrice;
 	localStorage.setItem("price", JSON.stringify(data));
 })
 
@@ -109,6 +126,7 @@ checkR450.addEventListener("change", (e) => {
 		R450 = 0;
 	}
 	req.innerText = "€ " + data.regPrice;
+	total1.value = data.regPrice;
 	localStorage.setItem("price", JSON.stringify(data));
 })
 
@@ -126,6 +144,7 @@ checkS70.addEventListener("change", (e) => {
 		S70 = 0;
 	}
 	std.innerText = "€ " + data.studPrice;
+	total2.value = data.studPrice;
 	localStorage.setItem("price", JSON.stringify(data));
 })
 
@@ -143,6 +162,7 @@ checkS200.addEventListener("change", (e) => {
 		S200 = 0;
 	}
 	std.innerText = "€ " + data.studPrice;
+	total2.value = data.studPrice;
 	localStorage.setItem("price", JSON.stringify(data));
 })
 
@@ -160,6 +180,7 @@ checkS400.addEventListener("change", (e) => {
 		S400 = 0;
 	}
 	std.innerText = "€ " + data.studPrice;
+	total2.value = data.studPrice;
 	localStorage.setItem("price", JSON.stringify(data));
 })
 
@@ -177,6 +198,7 @@ checkS550.addEventListener("change", (e) => {
 		S550 = 0;
 	}
 	std.innerText = "€ " + data.studPrice;
+	total2.value = data.studPrice;
 	localStorage.setItem("price", JSON.stringify(data));
 })
 
@@ -185,45 +207,36 @@ checkS550.addEventListener("change", (e) => {
 const submit_from = document.querySelector(".submit-form");
 
 submit_from.addEventListener("submit", (e) => {
-	e.preventDefault();
-	let data = new FormData(submit_from);
-	data.append("name", "Before 10th May 2024");
+e.preventDefault();
+	submit_from.submit()
+	localStorage.removeItem("price")
+	localStorage.setItem("price", JSON.stringify({ regPrice: 0, studPrice: 0 }));
+	let data = JSON.parse(localStorage.getItem("price"));
+	req.innerText = "€ " + data.regPrice;
+	std.innerText = "€ " + data.studPrice;
+	submit_from.reset();
+	// let jsonObject = {};
+	// data.forEach((value, key) => {
+	// 	jsonObject[key] = value;
+	// });
 
-	if (R70 === 1)
-		data.append("Social Event", "70");
-	if (R200 === 1)
-		data.append("Accompanying person", "200");
-	if (R300 === 1)
-		data.append("Student", "300");
-	if (R450 === 1)
-		data.append("Regular Participants", "450");
-
-	let jsonObject = {};
-	data.forEach((value, key) => {
-		jsonObject[key] = value;
-	});
-
-	console.log(jsonObject);
+	// console.log(jsonObject);
 });
 
 const submit_from2 = document.querySelector(".submit-form2");
 
 submit_from2.addEventListener("submit", (e) => {
-	e.preventDefault();
-	let data = new FormData(submit_from2);
-	data.append("name", "From 15th June 2024");
-	if (S70 === 1)
-		data.append("Social Event", "70");
-	if (S200 === 1)
-		data.append("Accompanying person", "200");
-	if (S400 === 1)
-		data.append("Student", "400");
-	if (S550 === 1)
-		data.append("Regular Participants", "550");
-
-	let jsonObject = {};
-	data.forEach((value, key) => {
-		jsonObject[key] = value;
-	});
-	console.log(jsonObject);
+e.preventDefault();
+	submit_from2.submit()
+	localStorage.removeItem("price")
+	localStorage.setItem("price", JSON.stringify({ regPrice: 0, studPrice: 0 }));
+	let data = JSON.parse(localStorage.getItem("price"));
+	req.innerText = "€ " + data.regPrice;
+	std.innerText = "€ " + data.studPrice;
+	submit_from2.reset()
+	// let jsonObject = {};
+	// data.forEach((value, key) => {
+	// 	jsonObject[key] = value;
+	// });
+	
 });
